0cdc78dfd2e0af68ce5eb3f27f522bf995529107
Alvine Chungo <134923016+Alvine-chungo969@users.noreply.github.com>
2026-07-25 17:57:51 +0300

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,45 +1,43 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiLinux, DiPython, DiGit } from "react-icons/di";
-import { SiWindows, SiCisco, SiWireshark, SiKalilinux, SiMicrosoftoffice, SiPowershell, SiVisualstudiocode } from "react-icons/si";
-import { FaNetworkWired } from "react-icons/fa";
+import { DiPython, DiGit } from "react-icons/di";
+import {
+  SiWindows11,
+  SiMicrosoftazure,
+  SiMicrosoftoffice,
+  SiSap,
+  SiPowershell,
+} from "react-icons/si";
+import { FaNetworkWired, FaTicketAlt, FaUsersCog, FaUserShield } from "react-icons/fa";
+
+const services = [
+  { icon: <FaTicketAlt />, label: "ServiceNow ITSM" },
+  { icon: <FaUsersCog />, label: "Active Directory" },
+  { icon: <SiMicrosoftazure />, label: "Azure & Intune" },
+  { icon: <SiMicrosoftoffice />, label: "Microsoft 365" },
+  { icon: <SiSap />, label: "SAP" },
+  { icon: <SiWindows11 />, label: "Windows OS" },
+  { icon: <FaNetworkWired />, label: "Networking" },
+  { icon: <SiPowershell />, label: "PowerShell" },
+  { icon: <DiPython />, label: "Python" },
+  { icon: <DiGit />, label: "Git" },
+  { icon: <FaUserShield />, label: "Cybersecurity" },
+];
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiWindows title="Windows OS" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiLinux title="Linux" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiKalilinux title="Kali Linux" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiCisco title="Cisco Networking" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiWireshark title="Wireshark" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaNetworkWired title="Networking" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython title="Python Scripting" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPowershell title="PowerShell Scripting" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit title="Git Version Control" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode title="VS Code" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMicrosoftoffice title="Microsoft Office" />
-      </Col>
+      {services.map((s) => (
+        <Col xs={6} md={3} lg={2} key={s.label}>
+          <div className="service-tile">
+            <div className="service-icon">{s.icon}</div>
+            <p className="service-label">{s.label}</p>
+            <span className="service-status">
+              <span className="status-dot"></span>Operational
+            </span>
+          </div>
+        </Col>
+      ))}
     </Row>
   );
 }
