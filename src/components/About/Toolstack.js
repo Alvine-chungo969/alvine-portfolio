6daee793c2b0849ac00a82727d4f937767cf6e56
Alvine Chungo <134923016+Alvine-chungo969@users.noreply.github.com>
2026-07-25 17:58:26 +0300

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,33 +1,37 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiVisualstudiocode, SiMicrosoftoffice, SiWireshark, SiVirtualbox, SiGithub } from "react-icons/si";
-import { FaNetworkWired } from "react-icons/fa";
+import {
+  SiVisualstudiocode,
+  SiWireshark,
+  SiVirtualbox,
+  SiGithub,
+  SiMicrosoftteams,
+} from "react-icons/si";
 import { MdDesktopWindows } from "react-icons/md";
+
+const tools = [
+  { icon: <SiVisualstudiocode />, label: "VS Code" },
+  { icon: <SiWireshark />, label: "Wireshark" },
+  { icon: <SiVirtualbox />, label: "VirtualBox" },
+  { icon: <MdDesktopWindows />, label: "Remote Desktop" },
+  { icon: <SiMicrosoftteams />, label: "Microsoft Teams" },
+  { icon: <SiGithub />, label: "GitHub" },
+];
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode title="Visual Studio Code" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMicrosoftoffice title="Microsoft Office Suite" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiWireshark title="Wireshark" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVirtualbox title="VirtualBox" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <MdDesktopWindows title="Remote Desktop Tools" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaNetworkWired title="Networking Tools" />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGithub title="GitHub" />
-      </Col>
+      {tools.map((t) => (
+        <Col xs={6} md={3} lg={2} key={t.label}>
+          <div className="service-tile">
+            <div className="service-icon">{t.icon}</div>
+            <p className="service-label">{t.label}</p>
+            <span className="service-status">
+              <span className="status-dot"></span>Operational
+            </span>
+          </div>
+        </Col>
+      ))}
     </Row>
   );
 }
